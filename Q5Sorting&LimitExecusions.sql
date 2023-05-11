9acb189609f2f062edd6895c3ee9bc665ad46407
mohammedakbar982 <133189360+mohammedakbar982@users.noreply.github.com>
2023-05-11 12:35:51 -0400

    Create Q5Sorting&LimitExecusions.sql

---
+++ Q5Sorting&LimitExecusions.sql
@@ -0,0 +1,6 @@
+SELECT place, COUNT(*) as num_earthquakes, AVG(mag) as avg_magnitude
+FROM earthquakes_table
+WHERE time BETWEEN '2022-01-01' AND '2023-05-11'
+GROUP BY place
+HAVING COUNT(*) >= 10
+ORDER BY num_earthquakes DESC;
